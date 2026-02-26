00c8337793d675cdc6bce9561dd97e3a727136d2
Lev Slinsen <lev_slinsen@protonmail.com>
2026-02-26 08:48:00 +0100

    .

--- config/settings/production.py
+++ config/settings/production.py
@@ -85,7 +85,7 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = EMAIL_SUBJECT_PREFIX
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
-ADMIN_URL = env("DJANGO_ADMIN_URL")
+# ADMIN_URL = env("DJANGO_ADMIN_URL")
 
 # Anymail
 # ------------------------------------------------------------------------------
